e23715cc89e7b61acb270314d5f20ffe54ca1eda
Haley Warson <haleywarson@gmail.com>
2021-01-30 16:49:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,5 @@
-# Write your solution here
-puts "Hey, it's 2019!"
-
-puts "You know what year it is??"
-this_year = 2019
-puts "Hey, it's 2019!" if this_year == 2019
-
-Time.now.year
-
-Time.now
+if true
+  puts "Even!"
+else
+  puts "Odd!"
+end
